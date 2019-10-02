52e565b6ef1d30d264a0d4049c000db3df781e28
MaevaBtd <melizenn@gmail.com>
2019-10-02 17:09:35 +0200

    3 files modified, 3 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'bonjour';
+  isAuth = false;
+  constructor() {
+    setTimeout(
+      ()=> {
+        this.isAuth = true;
+      }, 4000
+    )
+  }
+
+  onAllumer() {
+    console.log('On allume tout !');
+}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,21 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { MonPremierComponent } from './mon-premier/mon-premier.component';
+import { AppareilComponent } from './appareil/appareil.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, MonPremierComponent ],
-  bootstrap:    [ AppComponent ]
+  imports:      [ 
+    BrowserModule, 
+    FormsModule 
+    ],
+  declarations: [ 
+    AppComponent, 
+    HelloComponent, 
+    MonPremierComponent, AppareilComponent 
+    ],
+  bootstrap:    [ 
+    AppComponent 
+    ],
+  providers: [],
 })
 export class AppModule { }
